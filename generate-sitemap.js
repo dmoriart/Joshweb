d7b8d1c774f2202300e18b679fd57c93c9026b9e
dmoriart <desmond.moriarty@gmail.com>
2025-07-08 18:46:40 +0100

    Fixed sitemap generator to work with ES modules

--- generate-sitemap.js
+++ generate-sitemap.js
@@ -5,8 +5,12 @@
  * Run this script to update the sitemap.xml file
  */
 
-const fs = require('fs');
-const path = require('path');
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const DOMAIN = 'https://joshmoriartyfilms.ie';
 const TODAY = new Date().toISOString().split('T')[0];
